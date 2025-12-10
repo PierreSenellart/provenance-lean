c8e49d8bcbdc52bb80681bef83d45273f6a5aabf
Pierre Senellart <pierre@senellart.com>
2025-12-10 08:59:01 +0100

    How[X] is universal

--- Provenance/Semirings/How.lean
+++ Provenance/Semirings/How.lean
@@ -1,15 +1,16 @@
 import Mathlib.Algebra.MvPolynomial.Basic
+import Mathlib.Algebra.MvPolynomial.Eval
 import Mathlib.Data.Finsupp.ToDFinsupp
 
 import Provenance.SemiringWithMonus
 
-variable {α: Type} [DecidableEq α]
+variable {X: Type} [DecidableEq X]
 
-instance : LE (MvPolynomial α ℕ) where
+instance : LE (MvPolynomial X ℕ) where
   le a b := ∀ m, a.coeff m ≤ b.coeff m
 
 
-instance : Sub (MvPolynomial α ℕ) where
+instance : Sub (MvPolynomial X ℕ) where
   sub a b :=
     let aDF := a.toDFinsupp
     let bDF := b.toDFinsupp
@@ -17,7 +18,7 @@ instance : Sub (MvPolynomial α ℕ) where
     rDF.toFinsupp
 
 
-instance : PartialOrder (MvPolynomial α ℕ) where
+instance : PartialOrder (MvPolynomial X ℕ) where
   le_refl := by
     simp only[(· ≤ ·)]
     simp
@@ -32,7 +33,7 @@ instance : PartialOrder (MvPolynomial α ℕ) where
     exact le_trans (hab m) (hbc m)
 
 
-instance : IsOrderedAddMonoid (MvPolynomial α ℕ) where
+instance : IsOrderedAddMonoid (MvPolynomial X ℕ) where
   add_le_add_left := by
     intro a b hab c m
     rw[MvPolynomial.coeff_add]
@@ -40,7 +41,7 @@ instance : IsOrderedAddMonoid (MvPolynomial α ℕ) where
     exact hab m
 
 
-instance : CanonicallyOrderedAdd (MvPolynomial α ℕ) where
+instance : CanonicallyOrderedAdd (MvPolynomial X ℕ) where
   exists_add_of_le := by
     intro a b hab
     use Finsupp.zipWith (· - ·) (by simp) b a
@@ -59,7 +60,7 @@ instance : CanonicallyOrderedAdd (MvPolynomial α ℕ) where
 /-- Marked as noncomputable only because the proof that `MvPolynomial` is a
 `CommutativeSemiring` is done in a non-computable way in Mathlib. We
 could redefine `MvPolynomial` to provide computable proofs. -/
-noncomputable instance : SemiringWithMonus (MvPolynomial α ℕ) where
+noncomputable instance : SemiringWithMonus (MvPolynomial X ℕ) where
   monus_spec := by
     intro a b c
     apply Iff.intro
@@ -68,14 +69,23 @@ noncomputable instance : SemiringWithMonus (MvPolynomial α ℕ) where
     . intro h m
       exact Nat.sub_le_iff_le_add'.mpr (h m)
 
-theorem How.not_idempotent : ¬(idempotent (MvPolynomial α ℕ)) := by
+theorem How.not_idempotent : ¬(idempotent (MvPolynomial X ℕ)) := by
   simp
   use (MvPolynomial.C 1)
   rw[← MvPolynomial.C_add]
   rw[MvPolynomial.C_inj _ _]
   simp
 
-theorem How.not_absorptive : ¬(absorptive (MvPolynomial α ℕ)) := by
-  have h₁ := @idempotent_of_absorptive (MvPolynomial α ℕ) _
-  have h₂ : ¬(idempotent (MvPolynomial α ℕ)) := How.not_idempotent
+theorem How.not_absorptive : ¬(absorptive (MvPolynomial X ℕ)) := by
+  have h₁ := @idempotent_of_absorptive (MvPolynomial X ℕ) _
+  have h₂ : ¬(idempotent (MvPolynomial X ℕ)) := How.not_idempotent
   tauto
+
+/-- The How[X] semiring is universal among commutative semirings. This
+  was observed in [Green, Karnouvarakis, Tannen, *Provenance
+  Semirings*, Proposition 4.2][green2007provenance]. -/
+theorem How.universal (K : Type) [CommSemiring K] :
+  ∀ ν: X → K, ∃ h: RingHom (MvPolynomial X ℕ) K, ∀ i: X, h (MvPolynomial.X i) = ν i := by
+    intro ν
+    use MvPolynomial.eval₂Hom (Nat.castRingHom K) ν
+    simp[MvPolynomial.eval₂Hom_X']
